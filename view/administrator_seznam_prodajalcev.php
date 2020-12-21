bedbd16edcad07b54cf9f27efdbee0888a56a66a
rm3612 <72322126+rm3612@users.noreply.github.com>
2020-12-21 01:36:28 +0100

    Update administrator_seznam_prodajalcev.php

--- view/administrator_seznam_prodajalcev.php
+++ view/administrator_seznam_prodajalcev.php
@@ -44,10 +44,9 @@
 		<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 		
 		<!-- Search form -->
-		<form class="form-inline ml-auto">
-		<button class="btn btn-sm btn-outline-light my-2 my-sm-0" type="submit"> <i class="fas fa-search"></i></button>
-		<input class="form-control-sm ml-sm-1 " type="search" placeholder="Išči" aria-label="Search">
-		</form>
+		<div class="ml-auto">
+                    
+                </div>
 		
 		<!-- dropwdown meni -->
 		<ul class="navbar-nav ml-2">
